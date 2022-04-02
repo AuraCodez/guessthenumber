5d39d50a279ad4c04bcf8b01529ffd8f83d38511
farhan-a3 <92063517+farhan-a3@users.noreply.github.com>
2022-04-01 23:47:34 -0400

    removed text in the text field and added back button functionality

--- app/src/main/java/com/example/guessthenumber/EasyMode.java
+++ app/src/main/java/com/example/guessthenumber/EasyMode.java
@@ -2,14 +2,31 @@ package com.example.guessthenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class EasyMode extends AppCompatActivity {
+    private Button btnEasyBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_mode);
+
+        btnEasyBack = findViewById(R.id.btnEasyBack);
+        btnEasyBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openModeSelection();
+            }
+        });
+    }
+
+    public void openModeSelection() {
+        Intent intent = new Intent(this, ModeSelection.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/guessthenumber/HardMode.java
+++ app/src/main/java/com/example/guessthenumber/HardMode.java
@@ -2,14 +2,30 @@ package com.example.guessthenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HardMode extends AppCompatActivity {
+    private Button btnHardBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hard_mode);
+
+        btnHardBack = findViewById(R.id.btnHardBack);
+        btnHardBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openModeSelection();
+            }
+        });
+    }
+
+    public void openModeSelection() {
+        Intent intent = new Intent(this, ModeSelection.class);
+        startActivity(intent);
     }
 }
