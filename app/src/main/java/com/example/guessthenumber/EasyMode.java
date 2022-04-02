730f55d403c853894f09db409fa8239ed315be22
farhan-a3 <92063517+farhan-a3@users.noreply.github.com>
2022-04-02 02:21:08 -0400

    easy mode working

--- app/src/main/java/com/example/guessthenumber/EasyMode.java
+++ app/src/main/java/com/example/guessthenumber/EasyMode.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EasyMode extends AppCompatActivity {
     private Button btnEasyBack;
-
+    private Button btnEasyGuess;
+    private EditText txtEasyGuess;
+    private TextView lblEasyResponse;
+    private int randomNum = NumberGuessingGameModel.randomNumEasy();
+    private int guessCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,37 @@ public class EasyMode extends AppCompatActivity {
                 openModeSelection();
             }
         });
+
+        btnEasyGuess = findViewById(R.id.btnEasyGuess);
+        btnEasyGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enterGuess();
+            }
+        });
+
+        txtEasyGuess = findViewById(R.id.txtEasyGuess);
+        lblEasyResponse = findViewById(R.id.lblEasyResponse);
     }
 
     public void openModeSelection() {
         Intent intent = new Intent(this, ModeSelection.class);
         startActivity(intent);
+    }
+
+    public void enterGuess() {
+        if (NumberGuessingGameModel.isNumber(txtEasyGuess.getText().toString()) == false) {
+            lblEasyResponse.setText("Please enter a whole number!");
+        } else if (NumberGuessingGameModel.isWholeNumber(Double.parseDouble(txtEasyGuess.getText().toString())) == false) {
+            lblEasyResponse.setText("Please enter a whole number!");
+        } else {
+            if (Integer.parseInt(txtEasyGuess.getText().toString()) == randomNum) {
+                lblEasyResponse.setText("Correct, you win!");
+                guessCounter++;
+            } else {
+                lblEasyResponse.setText("Incorrect, try again!");
+                guessCounter++;
+            }
+        }
     }
 }
