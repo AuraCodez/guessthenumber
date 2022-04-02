00db55f6f4a2cbae0199ce6320dc15c63fa4574f
Ryan L <dankmemerkid000@gmail.com>
2022-04-02 01:21:18 -0400

    Added Credits Page

--- app/src/main/java/com/example/guessthenumber/Credits.java
+++ app/src/main/java/com/example/guessthenumber/Credits.java
@@ -7,32 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class EasyMode extends AppCompatActivity {
-    private Button btnEasyBack;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> ae718a51f766048a36fbc5842ddcb3ea1865fc41
-=======
->>>>>>> ae718a51f766048a36fbc5842ddcb3ea1865fc41
-
+public class Credits extends AppCompatActivity {
+    private Button btnCredits;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_easy_mode);
+        setContentView(R.layout.credits);
 
-        btnEasyBack = findViewById(R.id.btnEasyBack);
-        btnEasyBack.setOnClickListener(new View.OnClickListener() {
+        btnCredits = findViewById(R.id.btnCredits);
+        btnCredits.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openModeSelection();
             }
         });
     }
-
     public void openModeSelection() {
         Intent intent = new Intent(this, ModeSelection.class);
         startActivity(intent);
     }
 }
+
